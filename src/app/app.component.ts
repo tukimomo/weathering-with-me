442bbb82ea8ac66de76606761a26026a26772596
Trân Võ <vobao.tran2612@gmail.com>
2024-07-05 11:42:29 +0700

    feat: draft display list of weather forecasts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {OverviewInfoCardComponent} from "./components/overview-info-card/overview-info-card.component";
+import {SearchFormComponent} from "./components/search-form/search-form.component";
+import {WeatherForecastService} from "./core/service/weather-forecast.service";
+import {WeatherForecast} from "./core/models/weather-forecast";
+import {WeatherForecastProcessorService} from "./core/service/weather-forecast-processor.service";
+import {RawWeatherForecast} from "./core/models/raw-weather-forecast";
+import {AsyncPipe, DatePipe} from "@angular/common";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OverviewInfoCardComponent, SearchFormComponent, DatePipe, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'weathering-with-me';
+  weatherForecastService = inject(WeatherForecastService);
+  weatherForecastProcessorService = inject(WeatherForecastProcessorService);
+  forecasts = signal<WeatherForecast>({});
+  resultList = new BehaviorSubject<Array<{date: string; data: { [time: string]: RawWeatherForecast }}>>([]);
+
+  handleSearch(locationName: string) {
+    this.weatherForecastService.getWeatherForecastForFiveDays({
+      q: locationName,
+    }).subscribe(result => {
+      this.forecasts.set(this.weatherForecastProcessorService.processData(result.list));
+      this.getForecastList();
+    });
+  }
+
+  getForecastList(){
+    let result: Array<{date: string; data: { [time: string]: RawWeatherForecast }}> = [];
+    Object.entries(this.forecasts()).map(([date, value]) => result.push(
+      {date: date, data: value}
+    ));
+    // note: filter last item as it does not have all info
+    this.resultList.next(result.filter(i => result.indexOf(i) !== 5));
+  }
 }
